11a76c5db62def828cae40d12e220534508c02d3
Zafei-Erin <897659752@qq.com>
2023-12-29 18:28:27 +0800

    fix: blocks order and loader

--- src/pages/Explorer/components/Explorer/LatestBlocks.tsx
+++ src/pages/Explorer/components/Explorer/LatestBlocks.tsx
@@ -1,6 +1,7 @@
 import { ethers } from "ethers";
 import { Link } from "react-router-dom";
 import BoxIcon from "~/assets/BoxIcon";
+import { Loader } from "~/pages/Swap/components";
 
 type LatestBlockType = {
   blocks: ethers.providers.Block[] | undefined;
@@ -11,60 +12,73 @@ export const LatestBlocks: React.FC<LatestBlockType> = ({
   blocks,
   gasPrice,
 }) => {
+  
+  blocks?.sort((a, b) => {
+    return b.timestamp - a.timestamp;
+  });
+
   return (
     <div className="border rounded-lg divide-y shadow-lg shadow-gray-100">
       <div className="text-sm font-semibold py-4 px-3">Lastest Blocks</div>
-      <div className="px-3 divide-y">
-        {blocks &&
-          gasPrice &&
-          blocks.map((block, index) => {
-            // const blockReward = ethers.BigNumber.from(block.gasUsed)
-            //   .mul(gasPrice)
-            //   .toString();
-            //   const blockRewardFormatted = ethers.utils.formatUnits(blockReward, 18)
-            return (
-              <div
-                key={index}
-                className="flex justify-between items-center text-sm"
-              >
-                <div className="py-4 flex gap-2 items-center">
-                  <BoxIcon className="w-12 h-12 p-3 text-gray-600 bg-gray-100 rounded-lg" />
-                  <div>
-                    <Link to={`block/${block.number}`}>
-                      <div className="text-sky-600">{block.number}</div>
-                    </Link>
-                    <div className="text-gray-500">{block.timestamp}</div>
-                  </div>
-                </div>
-                <div>
-                  <div>
-                    Miner:{" "}
-                    <Link to={`account/${block.miner}`}>
-                      <div className="text-sky-600 hover:text-sky-700">
-                        {block.miner.slice(0, 8)}...{block.miner.slice(34)}
+      {!blocks ? (
+        <div className="h-[20rem] flex items-center justify-center">
+          <Loader className="w-20 h-20" />
+        </div>
+      ) : (
+        <>
+          <div className="px-3 divide-y">
+            {blocks &&
+              gasPrice &&
+              blocks.map((block, index) => {
+                // const blockReward = ethers.BigNumber.from(block.gasUsed)
+                //   .mul(gasPrice)
+                //   .toString();
+                //   const blockRewardFormatted = ethers.utils.formatUnits(blockReward, 18)
+                return (
+                  <div
+                    key={index}
+                    className="flex justify-between items-center text-sm"
+                  >
+                    <div className="py-4 flex gap-2 items-center">
+                      <BoxIcon className="w-12 h-12 p-3 text-gray-600 bg-gray-100 rounded-lg" />
+                      <div>
+                        <Link to={`block/${block.number}`}>
+                          <div className="text-sky-600">{block.number}</div>
+                        </Link>
+                        <div className="text-gray-500">{block.timestamp}</div>
                       </div>
-                    </Link>
-                  </div>
-                  <div className="flex gap-1">
-                    <Link to={`block/${block.number}`}>
-                      <div className="text-sky-600 hover:text-sky-700">
-                        {block.transactions.length} txns
+                    </div>
+                    <div>
+                      <div>
+                        Miner:{" "}
+                        <Link to={`account/${block.miner}`}>
+                          <div className="text-sky-600 hover:text-sky-700">
+                            {block.miner.slice(0, 8)}...{block.miner.slice(34)}
+                          </div>
+                        </Link>
                       </div>
-                    </Link>
-                    <div className="text-gray-400">in 12 secs</div>
-                  </div>
-                </div>
-                {/* <div className="border rounded-lg p-2 font-semibold text-xs">
+                      <div className="flex gap-1">
+                        <Link to={`block/${block.number}`}>
+                          <div className="text-sky-600 hover:text-sky-700">
+                            {block.transactions.length} txns
+                          </div>
+                        </Link>
+                        <div className="text-gray-400">in 12 secs</div>
+                      </div>
+                    </div>
+                    {/* <div className="border rounded-lg p-2 font-semibold text-xs">
                   {blockRewardFormatted} Eth
                 </div> */}
-              </div>
-            );
-          })}
-      </div>
+                  </div>
+                );
+              })}
+          </div>
 
-      <div className="flex items-center hover:cursor-pointer hover:text-sky-700 justify-center rounded-b-lg py-4 font-semibold text-gray-500 bg-gray-50 text-xs">
-        VIEW ALL BLOCKS
-      </div>
+          <div className="flex items-center hover:cursor-pointer hover:text-sky-700 justify-center rounded-b-lg py-4 font-semibold text-gray-500 bg-gray-50 text-xs">
+            VIEW ALL BLOCKS
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/pages/Explorer/components/Explorer/LatestTxns.tsx
+++ src/pages/Explorer/components/Explorer/LatestTxns.tsx
@@ -1,6 +1,7 @@
 import { ethers } from "ethers";
 import { Link } from "react-router-dom";
 import BoxIcon from "~/assets/BoxIcon";
+import { Loader } from "~/pages/Swap/components";
 
 type LatestTxnsType = {
   transaction: ethers.providers.TransactionReceipt[] | undefined;
@@ -12,49 +13,58 @@ export const LatestTxns: React.FC<LatestTxnsType> = ({ transaction }) => {
       <div className="text-sm font-semibold py-4 px-3">
         Lastest Transactions
       </div>
-      <div className="px-3 divide-y">
-        {transaction &&
-          transaction.map((txn, index) => (
-            <div
-              key={index}
-              className="flex justify-between items-center text-sm"
-            >
-              <div className="py-4 flex gap-2 items-center">
-                <BoxIcon className="w-12 h-12 p-3 text-gray-600 bg-gray-100 rounded-lg" />
-                <div>
-                  <Link to={`transaction/${txn}`}>
-                    <p className="text-sky-600 hover:text-sky-700">
-                      {txn.transactionHash.slice(0, 18)}...
-                    </p>
-                  </Link>
-                  <p className="text-gray-500">20 secs ago</p>
+
+      {!transaction ? (
+        <div className="h-[20rem] flex items-center justify-center">
+          <Loader className="w-20 h-20" />
+        </div>
+      ) : (
+        <>
+          <div className="px-3 divide-y">
+            {transaction &&
+              transaction.map((txn, index) => (
+                <div
+                  key={index}
+                  className="flex justify-between items-center text-sm"
+                >
+                  <div className="py-4 flex gap-2 items-center">
+                    <BoxIcon className="w-12 h-12 p-3 text-gray-600 bg-gray-100 rounded-lg" />
+                    <div>
+                      <Link to={`transaction/${txn}`}>
+                        <p className="text-sky-600 hover:text-sky-700">
+                          {txn.transactionHash.slice(0, 18)}...
+                        </p>
+                      </Link>
+                      <p className="text-gray-500">20 secs ago</p>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="flex gap-1">
+                      From
+                      <Link to={`account/${txn.from}`}>
+                        <p className="text-sky-600 hover:text-sky-700">
+                          {txn.from.slice(0, 8)}...{txn.from.slice(34)}
+                        </p>
+                      </Link>
+                    </div>
+                    <div className="flex gap-1">
+                      to
+                      <Link to={`account/${txn.from}`}>
+                        <p className="text-sky-600 hover:text-sky-700">
+                          {" "}
+                          {txn.to.slice(0, 8)}...{txn.to.slice(34)}
+                        </p>
+                      </Link>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div>
-                <div className="flex gap-1">
-                  From
-                  <Link to={`account/${txn.from}`}>
-                    <p className="text-sky-600 hover:text-sky-700">
-                      {txn.from.slice(0, 8)}...{txn.from.slice(34)}
-                    </p>
-                  </Link>
-                </div>
-                <div className="flex gap-1">
-                  to
-                  <Link to={`account/${txn.from}`}>
-                    <p className="text-sky-600 hover:text-sky-700">
-                      {" "}
-                      {txn.to.slice(0, 8)}...{txn.to.slice(34)}
-                    </p>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          ))}
-      </div>
-      <div className="flex items-center hover:cursor-pointer hover:text-sky-700 justify-center rounded-b-lg py-4 font-semibold text-gray-500 bg-gray-50 text-xs">
-        VIEW ALL TRANSACTIONS
-      </div>
+              ))}
+          </div>
+          <div className="flex items-center hover:cursor-pointer hover:text-sky-700 justify-center rounded-b-lg py-4 font-semibold text-gray-500 bg-gray-50 text-xs">
+            VIEW ALL TRANSACTIONS
+          </div>
+        </>
+      )}
     </div>
   );
 };
