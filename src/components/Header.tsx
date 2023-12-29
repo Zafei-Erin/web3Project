aa250168916473d83c2c768a63b1a880bf28533b
Zafei-Erin <897659752@qq.com>
2023-12-29 18:21:31 +0800

    fix: header padding and color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,15 +8,15 @@ const Header = () => {
       <header className=" h-16 items-center flex justify-between px-6 lg:max-w-[90%] mx-auto">
         <div className="flex items-center gap-6 py-2">
           <Link to={"/"}>
-            <LightLogo className="w-6 h-6" />
+            <LightLogo className="w-6 h-6 hover:text-amber-400 transition" />
           </Link>
           <Link to={"/swap"}>
-            <div className="rounded-md font-semibold items-center hover:bg-gray-100 hover:cursor-pointer">
+            <div className="rounded-md px-3 py-2 font-semibold transition items-center hover:bg-gray-100 hover:cursor-pointer">
               Swap
             </div>
           </Link>
           <Link to={"/explorer"}>
-            <div className="rounded-md font-semibold items-center hover:bg-gray-100 hover:cursor-pointer">
+            <div className="rounded-md px-3 py-2 font-semibold transition items-center hover:bg-gray-100 hover:cursor-pointer">
               Explorer
             </div>
           </Link>
